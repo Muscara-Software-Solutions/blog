c4069a1c722319d24dba4540e55f34d8776bf831
Kevin Muscara <muscarak1@nku.edu>
2023-05-24 19:17:40 -0400

    blog

---
+++ main.tsx
@@ -0,0 +1,11 @@
+/** @jsx h */
+
+import blog, { ga, redirects, h } from "blog";
+
+blog({
+  title: "Hallpass Blog",
+  description: "Welcome to the teacher's lounge, stay up to date with the latest stories, updates, tips & tracks, and so much more.",
+  avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
+  avatarClass: "rounded-full",
+  author: "Hallpass",
+});
---
+++ posts/hello_world.md
@@ -0,0 +1,7 @@
+---
+title: Hello world!
+abstract: First ever blog post
+publish_date: 2023-05-24
+---
+
+This is our first blog post from Hallpass!
