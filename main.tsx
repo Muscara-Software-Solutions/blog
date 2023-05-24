196b672c975a7e23ddd77ee4ce8785e75c3d507d
Kevin Muscara <muscarak1@nku.edu>
2023-05-24 19:34:28 -0400

    updated template with social links

--- main.tsx
+++ main.tsx
@@ -4,8 +4,15 @@ import blog, { ga, redirects, h } from "blog";
 
 blog({
   title: "Hallpass Blog",
-  description: "Welcome to the teacher's lounge, stay up to date with the latest stories, updates, tips & tracks, and so much more.",
+  author: "Hallpass",
   avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
   avatarClass: "rounded-full",
-  author: "Hallpass",
+  links: [
+    { title: "Email", url: "mailto:muscarasoftware@gmail.com" },
+    { title: "Home", url: "https://hallpass.gg" },
+    { title: "Instagram", url: "https://instagram.com" },
+    { title: "Twitter", url: "https://twitter.com" },
+    { title: "LinkedIn", url: "https://linkedin.com" },
+  ],
+  description: "Welcome to the teacher's lounge, stay up to date with the latest stories, updates, tips & tracks, and so much more.",
 });
